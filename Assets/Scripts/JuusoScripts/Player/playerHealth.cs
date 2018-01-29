3fd7e488c3e52729e5600155ec87843335f0243c
juumyllyla <31961349+juumyllyla@users.noreply.github.com>
2018-01-29 18:34:37 +0200

    Cleaning up project hierarchy.

---
+++ Assets/Scripts/JuusoScripts/Player/playerHealth.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class playerHealth : MonoBehaviour {
+
+    //public Sprite[] HealthSprites;
+
+    //public Image HeartUI;
+
+    public GameObject h1, h2, h3, h4, h5;
+
+    //public GameObject healthGUI;
+
+    public int hp = 5;
+
+    public bool flashActive = false;
+
+    public float flashLength;
+    private float flashCounter;
+
+    public SpriteRenderer playerSprite;
+
+    public Transform Explosion;
+
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        playerSprite = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //HeartUI.sprite = HealthSprites[hp];
+
+        if ((hp == 8 || hp == 9) && h5.activeInHierarchy == true)
+        {
+            //healthGUI = h4;
+            h5.SetActive(false);
+            h4.SetActive(true);
+        }
+        if (hp == 6 || hp == 7 && h4.activeInHierarchy == true)
+        {
+            //healthGUI = h3;
+            h4.SetActive(false);
+            h3.SetActive(true);
+        }
+        if (hp == 4 || hp == 3 && h3.activeInHierarchy == true)
+        {
+            //healthGUI = h2;
+            h3.SetActive(false);
+            h2.SetActive(true);
+        }
+        if (hp == 2 || hp == 1 && h2.activeInHierarchy == true)
+        {
+            //healthGUI = h1;
+            h2.SetActive(false);
+            h1.SetActive(true);
+        }
+
+        if (flashActive)
+        {
+            if (flashCounter > flashLength * .66f)
+            {
+                playerSprite.color = new Color(playerSprite.color.r, playerSprite.color.g, playerSprite.color.b, 0f);
+            }
+            else if (flashCounter > flashLength * .33f)
+            {
+                playerSprite.color = new Color(playerSprite.color.r, playerSprite.color.g, playerSprite.color.b, 1f);
+            }
+            else if (flashCounter > 0f)
+            {
+                playerSprite.color = new Color(playerSprite.color.r, playerSprite.color.g, playerSprite.color.b, 0f);
+            }
+            else
+            {
+                playerSprite.color = new Color(playerSprite.color.r, playerSprite.color.g, playerSprite.color.b, 1f);
+                flashActive = false;
+            }
+        }
+        flashCounter -= Time.deltaTime;
+
+        if (hp <= 0)
+        {
+            Instantiate(Explosion, transform.position, transform.rotation);
+            SceneManager.LoadScene("gameover");
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        //debug = true;
+        if (col.CompareTag("Bullet"))
+        {
+            hp--;
+            flashActive = true;
+            flashCounter = flashLength;
+        }
+        if (col.CompareTag("Child") || col.CompareTag ("Tank") || col.CompareTag("Melee"))
+        {
+            SceneManager.LoadScene("gameover");
+        }
+    }
+}
---
+++ Assets/Scripts/VeryUnfinished/RammerAttack.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RammerAttack : MonoBehaviour {
+
+    public Transform player;
+    private Transform target;
+
+    public float turnSpeed = 5;
+    public float runSpeed = .1f;
+    private float aggroRange = 20;
+    private float hitTimer = 2;
+
+    public Rigidbody rb;
+
+    private Ray Sight;
+
+    private bool hitting = false;
+    private bool turning = true;
+    private bool moving = false;
+
+    //Vector2 dir;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        Sight.origin = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        Sight.direction = transform.right;
+        RaycastHit hit;
+
+        if (Vector2.Distance(player.position, transform.position) < aggroRange)
+        {
+            //Vector2 dir = player.position - transform.position;
+            //dir.z = 0;
+            //Debug.Log(Vector2.Distance(player.position, transform.position));
+
+            if (turning == true)
+            {
+                Vector2 dir = player.position - transform.position;
+                float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+                Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+                transform.rotation = Quaternion.Slerp(transform.rotation, rotation, turnSpeed * Time.deltaTime);
+                Debug.DrawRay(Sight.origin, transform.right * aggroRange, Color.red);
+            }
+
+            if (Physics.Raycast(Sight, out hit, aggroRange))
+            {
+                // Debug.DrawRay(Sight.origin, transform.right * aggroRange, Color.red);
+                // print(hit.collider.tag);
+                if (hit.collider.tag == "Player")
+                {
+                    target = hit.transform;
+
+                    turning = false;
+                    moving = true;
+                    //rb.MovePosition(transform.position + transform.up * runSpeed * Time.smoothDeltaTime);
+                }
+            }
+
+            if (moving == true)
+            {
+                //transform.position = Vector3.MoveTowards(transform.position, target.position, runSpeed);
+                transform.Translate(runSpeed, 0, 0);
+
+                if (transform.position == target.position)  //Millä saa träkkäämään sitä positiota, jossa pelaaja oli sillä hetkellä kun Raycast osui?
+                    //Yksi vaihtoehto voisi olla, että rammeri luo näkymättömän objektin osumakohtaan ja lähtee sitten sitä kohti.
+                {
+                    Debug.Log("Noice, nyt oot siellä");
+                    if (hitting == false)
+                    {
+                        turning = true;
+                    }
+                }
+            }
+
+            //rb.MovePosition(transform.position + transform.up * runSpeed * Time.smoothDeltaTime);
+
+            //transform.Translate(runSpeed, 0, 0);
+
+            /*if (dir.magnitude > 5)
+            {
+                transform.Translate(0, 0, .05f);
+            }*/
+        }
+        /*if (hitTimer <= 0)
+        {
+        SILLON KUN ON TRANSFORMISSA TAI JOTAIN
+            hitting = false;
+            hitTimer = 2;
+        }*/
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            hitting = true;
+        }
+    }
+}
