ebcc75da846b00b05b3f8106fe4887788725975c
juumyllyla <31961349+juumyllyla@users.noreply.github.com>
2018-02-05 12:31:25 +0200

    Rammer AI advancements

--- Assets/Scripts/VeryUnfinished/RammerAttack.cs
+++ Assets/Scripts/VeryUnfinished/RammerAttack.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class RammerAttack : MonoBehaviour {
 
     public Transform player;
-    private Transform target;
+    //private Transform target;
+    private Transform rotateTo;
+
+    Vector3 targetV;
 
     public float turnSpeed = 5;
     public float runSpeed = .1f;
-    private float aggroRange = 20;
+    private float aggroRange = 10;
     private float hitTimer = 2;
 
     public Rigidbody rb;
@@ -20,11 +23,23 @@ public class RammerAttack : MonoBehaviour {
     private bool turning = true;
     private bool moving = false;
 
+    public GameObject targetObject;
+
+    float distance;
+    float distanceTravelled;
+    float wallDistance;
+
+    Vector2 lastPosition;
+
+    public Transform[] paths;
+
     //Vector2 dir;
 
     // Use this for initialization
-    void Start () {
-		
+    void Start ()
+    {
+        lastPosition = transform.position;
+        rotateTo = player;
 	}
 	
 	// Update is called once per frame
@@ -34,44 +49,79 @@ public class RammerAttack : MonoBehaviour {
         Sight.direction = transform.right;
         RaycastHit hit;
 
-        if (Vector2.Distance(player.position, transform.position) < aggroRange)
-        {
             //Vector2 dir = player.position - transform.position;
             //dir.z = 0;
             //Debug.Log(Vector2.Distance(player.position, transform.position));
 
-            if (turning == true)
+        if (turning == true)
+        {
+            Vector2 dir = rotateTo.position - transform.position;
+            float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+            Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, turnSpeed * Time.deltaTime);
+
+            transform.Translate(runSpeed, 0, 0);
+
+            /*GameObject[] walls = GameObject.FindGameObjectsWithTag("KillerWall");
+
+            foreach (GameObject wallObj in walls)
             {
-                Vector2 dir = player.position - transform.position;
-                float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-                Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-                transform.rotation = Quaternion.Slerp(transform.rotation, rotation, turnSpeed * Time.deltaTime);
+                wallDistance = Vector2.Distance(transform.position, wallObj.transform.position);
+            }*/
+
+            //wallDistance = Vector2.Distance(transform.position, GameObject.FindGameObjectsWithTag("KillerWall").transform.position);
+
+            //Vihollisen pitäis varmaan myös tyyliin kääntyä vähän eri suuntaan kun "törmää" seinään??
+            //Tai sitten pistetään vaan meneen jotain valmiiksi asetettua polkua pitkin... but that's kinda boring
+            //Tai sitten vähän yhdistellä molempia; jos törmää seinään, ohjataan polulle
+
+            if (Vector2.Distance(player.position, transform.position) < aggroRange)
+            {
                 Debug.DrawRay(Sight.origin, transform.right * aggroRange, Color.red);
-            }
 
-            if (Physics.Raycast(Sight, out hit, aggroRange))
-            {
-                // Debug.DrawRay(Sight.origin, transform.right * aggroRange, Color.red);
-                // print(hit.collider.tag);
-                if (hit.collider.tag == "Player")
+                if (Physics.Raycast(Sight, out hit, aggroRange))
                 {
-                    target = hit.transform;
+                    // Debug.DrawRay(Sight.origin, transform.right * aggroRange, Color.red);
+                    // print(hit.collider.tag);
+                    if (hit.collider.tag == "Player")
+                    {
+                        if (!targetObject.activeInHierarchy)
+                        {
+                            Instantiate(targetObject, player.position, transform.rotation);
+                            //targetV = new Vector3(targetObject.transform.position.x, targetObject.transform.position.y, targetObject.transform.position.z);
+                            //target = targetObject.transform;
 
-                    turning = false;
-                    moving = true;
-                    //rb.MovePosition(transform.position + transform.up * runSpeed * Time.smoothDeltaTime);
+                            turning = false;
+                            moving = true;
+                            distance = Vector2.Distance(transform.position, targetObject.transform.position);
+                        }
+                        if (targetObject.activeInHierarchy)
+                        {
+                            Destroy(targetObject);
+                        }
+                        //rb.MovePosition(transform.position + transform.up * runSpeed * Time.smoothDeltaTime);
+                    }
                 }
             }
 
             if (moving == true)
             {
+                //Debug.Log(target.position);
                 //transform.position = Vector3.MoveTowards(transform.position, target.position, runSpeed);
+                //distance = Vector2.Distance(transform.position, targetObject.transform.position);   //TÄMÄ JONNEKIN MUUALLE, MUUTEN EI TOIMI. SAA TAPAHTUA VAIN KERRAN!
+
+                distanceTravelled += Vector2.Distance(transform.position, lastPosition);
+                lastPosition = transform.position;
+
+                //Debug.Log("Dist: " + distance);
+                //Debug.Log("Dist travelled: " + distanceTravelled);
+
                 transform.Translate(runSpeed, 0, 0);
 
-                if (transform.position == target.position)  //Millä saa träkkäämään sitä positiota, jossa pelaaja oli sillä hetkellä kun Raycast osui?
+                if (distanceTravelled >= distance + 2)
                     //Yksi vaihtoehto voisi olla, että rammeri luo näkymättömän objektin osumakohtaan ja lähtee sitten sitä kohti.
                 {
-                    Debug.Log("Noice, nyt oot siellä");
+                    moving = false;
                     if (hitting == false)
                     {
                         turning = true;
@@ -101,6 +151,31 @@ public class RammerAttack : MonoBehaviour {
         if (collision.gameObject.tag == "Player")
         {
             hitting = true;
+        }
+        if (collision.gameObject.tag == "Wall")
+        {
+            Debug.Log("Hits well enough to feel it");
+            MoveToPoint();
+        }
+    }
+
+    public void MoveToPoint()
+    {
+        float minDist = Mathf.Infinity;
+        Transform tMin = null;
+
+        foreach (Transform pathPoint in paths)
+        {
+            float dist = Vector2.Distance(transform.position, pathPoint.position);
+
+            if (dist < minDist)
+            {
+                tMin = pathPoint;
+                rotateTo = tMin;
+
+                minDist = dist;
+            }
+            //wallDistance = Vector2.Distance(transform.position, wallObj.transform.position);
         }
     }
 }
---
+++ Assets/TimedDeath.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimedDeath : MonoBehaviour {
+
+    public float timedDeath;
+
+	// Use this for initialization
+	void Start ()
+    {
+        
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        timedDeath -= Time.deltaTime;
+
+        if (timedDeath <= 0)
+        {
+            Destroy(gameObject);
+        }
+	}
+}
